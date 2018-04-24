bb44903dc0abde0bceafe12e40d46cd87582ce00
Harbinder Kharod <hkharod@gmail.com>
2018-04-24 21:56:15 +0000

    create migrations to coincide with rss schema

--- database/migrations/2018_02_13_052447_create_items_table.php
+++ database/migrations/2018_02_13_052447_create_items_table.php
@@ -15,24 +15,16 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('source')->nullable();
-            $table->string('main_keyword')->nullable();
-            $table->string('job_title')->nullable();
-            $table->string('position_tags')->nullable();
-            $table->string('remote_tags')->nullable();
-            $table->string('tech_tags')->nullable();
-            $table->string('formatted_title')->nullable();
-            $table->string('company')->nullable();
-            $table->string('budget')->nullable();
-            $table->longText('job_html')->nullable();
-            $table->string('contact_link')->nullable();
-            $table->string('contact_text')->nullable();
-            $table->string('status')->nullable();
-            $table->string('emails')->nullable();
-            $table->string('filename')->nullable();
-            $table->string('scrape_status')->nullable();
-            $table->string('url')->nullable();
+            $table->integer('source_id');
+            $table->string('source_title');
+            $table->string('title')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('link')->nullable();
             $table->string('post_date')->nullable();
+            $table->string('comments')->nullable();
+            $table->string('category')->nullable();
+            $table->string('author')->nullable();
+            $table->string('guid')->nullable();
             $table->timestamps();
         });
     }
